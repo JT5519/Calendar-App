c276ff1a5a95602a90b66d750de3e5225d342656
JT5519 <joel.sk10@gmail.com>
2020-12-27 20:49:40 +0530

    edit

---
+++ app/src/main/java/com/example/kalnir_naya/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.kalnir_naya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    CustomCalendarView customCalendarView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        customCalendarView = (CustomCalendarView)findViewById(R.id.custom_calendar_view);
+    }
+}
